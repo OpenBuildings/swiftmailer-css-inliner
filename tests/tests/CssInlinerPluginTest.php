f5f12299406607722d3e560790a8f2f12fcfc142
Dawid Nowak <contact@dnowak.pl>
2014-04-14 18:10:41 +0200

    PHPUnit tests: email contents put into "fixtures" files instead of hardcoding them – it should make easier to add more test emails

--- tests/tests/CssInlinerPluginTest.php
+++ tests/tests/CssInlinerPluginTest.php
@@ -3,64 +3,19 @@
 use Openbuildings\Swiftmailer\CssInlinerPlugin;
 
 /**
- * @group   css-inliner-plugin
+ * @group css-inliner-plugin
  */
-class CssInlinerPluginTest extends PHPUnit_Framework_TestCase {
+class CssInlinerPluginTest extends PHPUnit_Framework_TestCase
+{
+	/**
+	 * @var string
+	 */
+	private $email_raw;
 
-	public $html = <<<HTML
-<html>
-	<head>
-		<style>
-			.block {
-				width: 100px;
-				height: 20px;
-			}
-			div.block ul li.small {
-				margin: 10px;
-			}
-		</style>
-	</head>
-	<body>
-		<div class="block">
-			text
-
-			<ul>
-				<li>
-					Big list
-				</li>
-				<li class="small">
-					Small list
-				</li>
-			</ul>
-		</div>
-	</body>
-</html>
-HTML;
-
-	public $converted_html = <<<CONVERTED
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/REC-html40/loose.dtd">
-<html><head><style>
-			.block {
-				width: 100px;
-				height: 20px;
-			}
-			div.block ul li.small {
-				margin: 10px;
-			}
-		</style></head><body>
-		<div class="block" style="height: 20px; width: 100px;">
-			text
-
-			<ul><li>
-					Big list
-				</li>
-				<li class="small" style="margin: 10px;">
-					Small list
-				</li>
-			</ul></div>
-	</body></html>
-
-CONVERTED;
+	/**
+	 * @var string
+	 */
+	private $email_converted;
 
 	public function test_html_body()
 	{
@@ -74,13 +29,12 @@ CONVERTED;
 		$message->setTo('test2@example.com');
 		$message->setSubject('Test');
 		$message->setContentType('text/html');
-		$message->setBody($this->html);
+		$message->setBody($this->email_raw);
 
 		$mailer->send($message);
-		
-		$this->assertEquals($this->converted_html, $message->getBody());
-	}
 
+		$this->assertEquals($this->email_converted, $message->getBody());
+	}
 
 	public function test_html_part()
 	{
@@ -93,13 +47,21 @@ CONVERTED;
 		$message->setFrom('test@example.com');
 		$message->setTo('test2@example.com');
 		$message->setSubject('Test');
-		$message->addPart($this->html, 'text/html');
+		$message->addPart($this->email_raw, 'text/html');
 		$message->addPart('plain part', 'text/plain');
 
 		$mailer->send($message);
 
 		$children = $message->getChildren();
-		
-		$this->assertEquals($this->converted_html, $children[0]->getBody());
+
+		$this->assertEquals($this->email_converted, $children[0]->getBody());
+	}
+
+	protected function setUp()
+	{
+		$dir = __DIR__.'/../fixtures/';
+
+		$this->email_raw = file_get_contents($dir.'email_raw.html');
+		$this->email_converted = file_get_contents($dir.'email_converted.html');
 	}
 }
